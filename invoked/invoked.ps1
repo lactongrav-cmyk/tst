e13ea936434c44c3b82a2807ab7440cfe5dd9d37
lactongrav-cmyk <lactongrav@gmail.com>
2025-10-01 11:22:18 +0300

    Update invoked.ps1

--- invoked/invoked.ps1
+++ invoked/invoked.ps1
@@ -1,5 +1,5 @@
 Write-Host "=== Тестовый payload успешно выполнен! ===" -ForegroundColor Green
 $computerName = $env:COMPUTERNAME
 $currentTime = Get-Date
-"Тест выполнен на компьютере: $computerName в $currentTime" | Out-File -FilePath "$env:C:\Users\tarin\Desktop\test_report.txt"
-Write-Host "Результат записан в $env:C:\Users\tarin\Desktop\test_report.txt"
+"Тест выполнен на компьютере: $computerName в $currentTime" | Out-File -FilePath "C:\Users\tarin\Desktop\test_report.txt"
+Write-Host "Результат записан в C:\Users\tarin\Desktop\test_report.txt"
